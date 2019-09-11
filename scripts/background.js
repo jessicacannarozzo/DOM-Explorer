04133f543c4ab0b9a8c288476229ec8eded4b8d8
jesscannarozzo <jesscannarozzo@cmail.carleton.ca>
2019-09-11 12:35:27 -0400

    Change to browser_action so that extension is always visible

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,9 @@
+/**
+ * the extension's event handler; it contains listeners for browser events that are important to the extension. 
+ * It lies dormant until an event is fired then performs the instructed logic.
+ * An effective background script is only loaded when it is needed and unloaded when it goes idle.
+ */
+
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({
         color: '#3aa757'
@@ -6,11 +12,15 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 
+chrome.declarativeContent.onClicked.addListener(function(tab) {
+    console.log("Hello");
+});
+
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {
-                hostEquals: 'developer.chrome.com'
+                hostEquals: 'google'
             },
         })],
         actions: [new chrome.declarativeContent.ShowPageAction()]
--- scripts/popup.js
+++ scripts/popup.js
@@ -5,11 +5,15 @@ chrome.storage.sync.get('color', function (data) {
     changeColor.setAttribute('value', data.color);
 });
 
-changeColor.onclick = function(element) {
+changeColor.onclick = function (element) {
     let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id, {
+                code: 'document.body.style.backgroundColor = "' + color + '";'
+            });
     });
-  };
+};
