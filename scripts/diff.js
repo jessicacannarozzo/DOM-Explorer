e87eeb25c20e80668112817259453b99f4aa2c6e
jesscannarozzo <jesscannarozzo@cmail.carleton.ca>
2019-10-08 13:46:33 -0400

    Icon change, also some initial css styling for popup.html

--- scripts/diff.js
+++ scripts/diff.js
@@ -11,3 +11,9 @@ function component() {
   
   document.body.appendChild(component());
   
+chrome.runtime.sendMessage({
+    greeting: "hello"
+},
+function (response) {
+    console.log(response.farewell)
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,25 @@
-let changeColor = document.getElementById('changeColor');
+let recordDOM = document.getElementById('recordDOM');
 
 chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+    recordDOM.style.backgroundColor = data.color;
+    recordDOM.setAttribute('value', data.color);
 });
 
-changeColor.addEventListener('click', element => {
+recordDOM.addEventListener('click', element => {
     let color = element.target.value;
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, tabs => {
-        console.log(document)
-        chrome.storage.sync.get([String(tabs.url)], function(result) {
-            console.log(result)
-        });
-        chrome.tabs.executeScript({
-            file: '/scripts/content.js'
-        });
-    });
+    // chrome.tabs.query({
+    //     active: true,
+    //     currentWindow: true
+    // }, tabs => {
+    //     console.log(document)
+    //     chrome.storage.sync.get([String(tabs.url)], function(result) {
+    //         console.log(result)
+    //     });
+    //     chrome.tabs.executeScript({
+    //         file: '/scripts/content.js'
+    //     });
+    // });
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "./diff.js"
 });
