6656357f418ca6942f815d5588b36a3b386bc90a
jesscannarozzo <jesscannarozzo@cmail.carleton.ca>
2019-10-09 16:04:19 -0400

    Set-up storage for storing the DOM object

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,11 @@
 /**
+ * @author Jess Cannarozzo
  * the extension's event handler; it contains listeners for browser events that are important to the extension. 
  * It lies dormant until an event is fired then performs the instructed logic.
  * An effective background script is only loaded when it is needed and unloaded when it goes idle.
  */
 
 chrome.runtime.onInstalled.addListener(() => {
-    // chrome.storage.sync.set({
-    //     color: '#3aa757'
-    // }, () => {
-    //     console.log("The color is green.");
-    // });
-    //rule to make button only work if the page is bookmarked
     var rule = {
         conditions: [
             new chrome.declarativeContent.PageStateMatcher({
@@ -26,10 +21,27 @@ chrome.runtime.onInstalled.addListener(() => {
 
 //receive DOM from content.js
 //store as url: {currentDOM, prevDOM}
+//first have to query for url in storage to see if there is already a currentDOM
+//TODO: what to respond back with?
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        // console.log(sender.tab ?
-        //     "from a content script " + sender.tab.url :
+        if (sender.tab) {
+            chrome.storage.local.get([sender.tab.url], function(urlDOM) {
+                console.log("Value is currently: " + urlDOM.key)
+                //if undefined, store new DOM at currentDOM
+                if (urlDOM.key === undefined) {
+                    var url = sender.tab.url;
+                    console.log(url);
+                    chrome.storage.local.set({
+                        url: {currentDOM: request.currentDOM}
+                    }, function() {
+                        console.log("Initialized new storage object.");
+                    })
+                } else { //if not undefined, move currentDOM to prevDOM and set new DOM to currentDOM
+
+                }
+            });
+        }
         sendResponse({
             farewell: "goodbye"
         });
