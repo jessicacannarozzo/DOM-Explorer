e2d58ca76a10f4c825ddea3509c3ab18b27595b8
jesscannarozzo <jesscannarozzo@cmail.carleton.ca>
2019-11-04 19:28:52 -0500

    Visual update

--- scripts/popup.js
+++ scripts/popup.js
@@ -16,6 +16,7 @@ recordDOM.addEventListener('click', element => {
     });
 });
 
+// on message received, do something iff request.DIFF exists (i.e. content.js is sending a diff)
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
         if (!sender || !sender.tab) {
@@ -28,10 +29,25 @@ chrome.runtime.onMessage.addListener(
             sendResponse({
                 success: true
             });
+            updatePopup(request.DIFF);
             return true;
         }        
+    }
+);
 
-    });
+// update popup.html after diff has been received
+function updatePopup(diff) {
+    if (diff != "") {
+
+        for (key in diff) {
+            console.log(diff[key])
+        }
+
+        document.getElementById("diff").innerHTML = JSON.stringify(diff[0]);
+    } else {
+        document.getElementById("diff").innerHTML = "No changes yet."
+    }
+}
 
 // var x = document.createElement("P");                        // Create a <p> element
 // var t = document.createTextNode("This is a paragraph.");    // Create a text node
